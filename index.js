79378079128eddc95a35b52446f2004e3caa45ec
Levy <edgarlevy81@gmail.com>
2023-07-21 17:23:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//remove main
+main.remove();
+
+//new id (newHeader) h1
+const newHeader = document.createElement('h1')
+
+// new head is "victory"
+newHeader.id = 'victory'
+
+//sweet msg (using innerHTML)
+newHeader.innerHTML = 'DeMarcus is the champion'
